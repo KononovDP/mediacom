db1d8ab21f5d59431aaf8339186c53ac05b5d700
dmitriy.kononov <kononov.d.p@gmail.com>
2020-07-20 10:46:35 +0300

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="wrapper">
+    <header class="header">Task Manager</header>
+    <main class="main">
+      <router-view/>
+    </main>
+    <footer class="footer">Footer</footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="sass">
+  * 
+    margin: 0
+    padding: 0
+  html, body
+    height: 100%
+  .wrapper
+    display: flex
+    flex-direction: column
+    height: 100%
+  .header
+    flex: 0 0 auto
+    padding: 15px 0
+    text-align: center
+    box-shadow: 0 3px 5px 0 rgba(0,0,0,.3)
+  .main
+    flex: 1 1 100%
+    overflow: auto
+  .footer
+    flex: 0 0 auto
+    padding: 15px 0
+    text-align: center
+    box-shadow: 0 -3px 5px 0 rgba(0,0,0,.3)
 </style>
